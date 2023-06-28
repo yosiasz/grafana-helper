e2f3fc3232182c5d7167f64daeaa41122001d48f
ዮስያስ <yosiasz@gmail.com>
2023-06-28 07:23:29 -0700

    feat(alerts) initial commit for alert dummy app

---
+++ src/alerts/grafana-alerts/src/app/page.tsx
@@ -0,0 +1,99 @@
+import styles from './page.module.css'
+//import { GridList, GridListTile } from "@material-ui/core";
+import { Grid } from '@mui/material';
+ 
+const stacks = [
+  { title: 'Slack', isFruit: false, id: 1 },
+  { title: 'Teams', isFruit: false, id: 2 },
+  { title: 'Telegram', isFruit: true, id: 3 },
+];
+
+export default function Home() {
+  
+  const listItems = stacks.map(stack =>
+    <li
+      key={stack.id}
+      style={{
+        color: stack.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {stack.title}
+    </li>
+  );  
+  return (
+    <main className={styles.main}>
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>      
+      </div>
+      <div>
+        <ul>{listItems}</ul>   
+      </div>        
+      <div className={styles.description}>
+      </div>
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
